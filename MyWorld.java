78bef3e0f4561f8eeb08b87de8ebbc5abb34fbd8
AustinYip <101162675+AustinYip@users.noreply.github.com>
2022-05-01 19:29:48 -0400

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,8 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        Seal seal1 = new Seal();
+        addObject(seal1,50,200);
     }
 }
